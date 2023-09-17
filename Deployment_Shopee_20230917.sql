1aad6e5dd8991adf3f09ab56372ce9c8f3da362d
BuzzDyne <buzzdyne@gmail.com>
2023-09-17 19:48:18 +0700

    Shopee Development

---
+++ Deployment_Shopee_20230917.sql
@@ -0,0 +1,14 @@
+USE sql_hcx;
+
+ALTER TABLE hcxprocesssyncstatus_tm
+ADD COLUMN access_token VARCHAR(32),
+ADD COLUMN refresh_token VARCHAR(32),
+ADD COLUMN refresh_token_expire_YYYYMMDD VARCHAR(32);
+
+ALTER TABLE orderitem_tr
+ADD COLUMN thumb_url VARCHAR(2000);
+
+INSERT INTO hcxprocesssyncstatus_tm(platform_name)
+SELECT "SHOPEE"
+FROM dual
+WHERE NOT EXISTS (SELECT * FROM hcxprocesssyncstatus_tm WHERE platform_name = "SHOPEE");
