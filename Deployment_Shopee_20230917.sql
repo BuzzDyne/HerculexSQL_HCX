c201281baa84746499d45c0156d46f81ee6167a8
BuzzDyne <buzzdyne@gmail.com>
2023-09-30 21:26:55 +0700

    Push Added Alter orderitem_tr

--- Deployment_Shopee_20230917.sql
+++ Deployment_Shopee_20230917.sql
@@ -8,6 +8,9 @@ ADD COLUMN refresh_token_expire_YYYYMMDD VARCHAR(32);
 ALTER TABLE orderitem_tr
 ADD COLUMN thumb_url VARCHAR(2000);
 
+ALTER TABLE orderitem_tr
+MODIFY ecom_order_id VARCHAR(100);
+
 INSERT INTO hcxprocesssyncstatus_tm(platform_name)
 SELECT "SHOPEE"
 FROM dual
