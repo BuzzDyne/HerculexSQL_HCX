9b9ec89b53877d542748062ff43f078622eea9ab
BuzzDyne <buzzdyne@gmail.com>
2024-05-10 21:01:28 +0700

    Add active flag for Orderanku Tables

--- 20240508_Orderanku.sql
+++ 20240508_Orderanku.sql
@@ -14,12 +14,22 @@ CREATE TABLE IF NOT EXISTS orderanku_item_tm (
     created_date		DATETIME,
     print_date          DATETIME,
     paid_date           DATETIME,
-    seller_id           INT    
+    seller_id           INT,    
+    is_active           BIT DEFAULT 1
 );
 
 CREATE TABLE IF NOT EXISTS orderanku_seller_tr (
     id                  INT AUTO_INCREMENT PRIMARY KEY,
     seller_name         VARCHAR(100),
-    seller_phone        VARCHAR(20)
-)
+    seller_phone        VARCHAR(20),
+    is_active           BIT DEFAULT 1
+);
 
+
+-- Parameter
+INSERT INTO orderanku_seller_tr (id, seller_name, seller_phone, is_active)
+VALUES (1, 'Herculex', '08151912345', 1) AS new
+ON DUPLICATE KEY UPDATE
+seller_name = new.seller_name,
+seller_phone = new.seller_phone,
+is_active = new.is_active;
